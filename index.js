93307f3be240dc5c248365dfe5a787582124c01a
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-06-24 20:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = []
+  for (let i = 0; i < musicians.length; i++) {
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return emptyArray
+}
+
+function johnLennonFacts(array) {
+  var storeArray = []
+  let i = 0
+  while (i < array.length) {
+    storeArray.push(`${array[i]}!!!`);
+    ++i;
+  }
+  return storeArray
+}
+
+function iLoveTheBeatles(number) {
+  let newArray = []
+  do {
+    newArray.push("I love the Beatles!");
+    ++number;
+  }
+    while (number < 15);
+    
+    return newArray;
+}
